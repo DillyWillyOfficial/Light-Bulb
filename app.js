7c4be316aac49d6b757e994732439c1c1f045315
TXT-92 <txt.hackathon@TXTs-MBP-2.lan>
2023-06-26 23:10:16 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const bulb=document.getElementById("bulb")
+function bulbOn(){
+    bulb.src="https://cdn1.vectorstock.com/i/1000x1000/05/10/3d-realistic-turning-on-light-bulb-icon-vector-28050510.jpg"
+}
+function bulbOff(){
+    bulb.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL0MEENCA-aSCYFol-VN6EaBrOcEimUWJD9fhdOuPi&s"
+}
